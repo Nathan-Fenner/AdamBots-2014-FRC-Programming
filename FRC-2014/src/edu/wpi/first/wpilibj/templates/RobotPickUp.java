dd753a2b11cea878610d76ec3075aaa8ea3f06be
Tyler <Tyler@Tyler-PC>
2014-01-20 11:57:57 -0500

    Created a new class, RobotPickUp.

---
+++ FRC-2014/src/edu/wpi/first/wpilibj/templates/RobotPickUp.java
@@ -0,0 +1,63 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package edu.wpi.first.wpilibj.templates;
+
+import edu.wpi.first.wpilibj.DigitalInput;
+import edu.wpi.first.wpilibj.Encoder;
+
+/**
+ *
+ * @author Tyler
+ */
+public class RobotPickUp {
+    
+////VARIABLES-------------------------------------------------------------------
+    private DigitalInput top1;
+    private DigitalInput top2;
+    private DigitalInput bot1;
+    private DigitalInput bot2;
+    private DigitalInput check;
+    private Encoder encoder;
+    
+    
+////CONSTANTS-------------------------------------------------------------------
+    public static final int PORT_TOP1 = 1;
+    public static final int PORT_TOP2 = 1;
+    public static final int PORT_BOT1 = 1;
+    public static final int PORT_BOT2 = 1;
+    public static final int PORT_CHECK = 1;
+    
+    public void init() {
+	//INSERT CODE HERE
+    }
+    
+    public void ifLoaded() {
+	//INSERT CODE HERE
+    }
+    
+    public void rollInOut() {
+	//INSERT CODE HERE
+    }
+    
+    public void removeTopArm() {
+	//INSERT CODE HERE
+    }
+    
+    public void pass() {
+	//INSERT CODE HERE
+	//?
+    }
+    
+    public void armMovement() {
+	//INSERT CODE HERE
+    }
+    
+    
+    
+    
+    
+}
