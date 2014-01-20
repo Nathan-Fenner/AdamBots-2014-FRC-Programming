7789fe5f16166f09c59e84f5892954155577317b
Tyler <Tyler@Tyler-PC>
2014-01-20 11:59:15 -0500

    Created a new class, RobotActuators.

---
+++ FRC-2014/src/edu/wpi/first/wpilibj/templates/RobotActuators.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package edu.wpi.first.wpilibj.templates;
+
+import edu.wpi.first.wpilibj.Victor;
+
+/**
+ *
+ * @author Tyler
+ */
+public class RobotActuators {
+    
+////VARIABLES-------------------------------------------------------------------
+    public static final Victor leftDrive;
+    public static final Victor rightDrive;
+    public static final Victor gamePieceIntake;
+    public static final Victor armRotate;
+    public static final Victor cockingMech;
+    
+    
+////CONSTANTS-------------------------------------------------------------------
+    
+    
+    public void init() {
+	//INSERT CODE HERE
+    }
+    
+}
