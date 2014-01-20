5645d3dd07b962ce8dee466aa408cdb630264311
Tyler <Tyler@Tyler-PC>
2014-01-20 11:58:59 -0500

    Created a new class, RobotSensors.

--- FRC-2014/src/edu/wpi/first/wpilibj/templates/RobotSensors.java
+++ FRC-2014/src/edu/wpi/first/wpilibj/templates/RobotSensors.java
@@ -6,10 +6,39 @@
 
 package edu.wpi.first.wpilibj.templates;
 
+import edu.wpi.first.wpilibj.ADXL345_I2C;
+import edu.wpi.first.wpilibj.DigitalInput;
+import edu.wpi.first.wpilibj.Encoder;
+import edu.wpi.first.wpilibj.Gyro;
+
 /**
  *
  * @author Tyler
  */
 public class RobotSensors {
+    
+////VARIABLES-------------------------------------------------------------------
+    public static final DigitalInput topLimit1;
+    public static final DigitalInput topLimit2;
+    public static final DigitalInput bottomLimit1;
+    public static final DigitalInput bottomLimit2;
+    public static final DigitalInput ballCheck;
+    public static final DigitalInput cockBack;
+    public static final DigitalInput releaseMech;
+    public static final Encoder cockingEnc;
+    public static final Encoder armEnc;
+    public static final Encoder leftStraightEnc;
+    public static final Encoder rightStraightEnc;
+    public static final ADXL345_I2C accel;
+    public static final Gyro gyro;
+    
+    
+    
+    
+////CONSTANTS-------------------------------------------------------------------
+    
+    public void init() {
+	//INSERT CODE HERE
+    }
     
 }
