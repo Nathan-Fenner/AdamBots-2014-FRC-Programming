aa9355c765811414aee7c2f2fd7f0331545ee41a
Tyler <Tyler@Tyler-PC>
2014-01-20 11:58:14 -0500

    Created a new class, RobotLights

---
+++ FRC-2014/src/edu/wpi/first/wpilibj/templates/RobotLights.java
@@ -0,0 +1,57 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package edu.wpi.first.wpilibj.templates;
+import edu.wpi.first.wpilibj.DigitalInput;
+/**
+ *
+ * @author Tyler
+ */
+public class RobotLights {
+    
+////VARIABLES-------------------------------------------------------------------
+    private DigitalInput liar;
+    
+    
+    
+////CONSTANTS-------------------------------------------------------------------
+    
+    public void init() {
+	//INSERT CODE HERE
+    }
+    
+    public void shootingLights() {
+	//INSERT CODE HERE
+    }
+    
+    public boolean ifReady() {
+	//INSERT CODE HERE
+	return
+    }
+    
+    public boolean ifJammed() {
+	//INSERT CODE HERE
+	return
+    }
+    
+    public void lightResponse() {
+	boolean ifReady = ifReady();
+	boolean ifJammed = ifJammed();
+	//INSERT CODE HERE
+    }
+    
+    public void underglow() {
+	//INSERT CODE HERE
+    }
+    
+    public void cameraSpotlight() {
+	//INSERT CODE HERE
+    }
+    
+    public void liar() {
+	//INSERT CODE HERE
+    }
+}
