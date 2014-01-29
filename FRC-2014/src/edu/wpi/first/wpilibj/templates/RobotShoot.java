b326513093275e5dc8dea4aeb7a913a2d4765dc1
Roi Orzach <roiorzach2@gmail.com>
2014-01-29 16:25:18 -0500

    RobotShoot
    Finished RobotShoot

--- FRC-2014/src/edu/wpi/first/wpilibj/templates/RobotShoot.java
+++ FRC-2014/src/edu/wpi/first/wpilibj/templates/RobotShoot.java
@@ -7,12 +7,13 @@ package edu.wpi.first.wpilibj.templates;
 import edu.wpi.first.wpilibj.*;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 
+
 /**
  *
  * @author Nathan
  */
 public class RobotShoot {
-    public static boolean limitshooter;
+    public static boolean limitShooter;
     public static boolean limitLatched;
     public static boolean limitBuckleValue;
     public static double WIND_SPEED;
@@ -29,9 +30,10 @@ public class RobotShoot {
     public static Encoder shooterEncoder;
     */
     public static int revolutionsOfShooter;
-    public static double a;
+    public static double time;
     public static double b;
     public static double rewindMaxRevolutions = 5;
+    
     /**
      * This method returns the tension (in encoder ticks or something) in terms
      * of the distance in feet to the target.
@@ -43,20 +45,21 @@ public class RobotShoot {
     /**
      * this will initialize the victors limit switches and pneumatics
      */
-    public static void init() {
-        a = 0;
+    public static void init() {  //this is the initialization method-surprise surpriise
+        timerLatch = new Timer();
+        time = 0;
         b = 0;
     }
     
-    public static void AutomatedShoot(){
+    public static void AutomatedShoot(){  //this does an automatic shot and reload
         RobotShoot.update();
         RobotShoot.releaseBall();
         RobotShoot.unwindShooter();
         RobotShoot.rewindShooter();
     }
-    public static void releaseBall() {
-        if (RobotPickup.isBallLoaded()) {
-            RobotSensors.latchSolenoid.set(false);
+    public static void releaseBall() {  //this will release the ball when it is loaded
+        if (RobotPickUp.ifLoaded()) {
+            RobotActuators.LATCH.set(false);
             
         } else {
             SmartDashboard.putString("Error", "Ball not loaded");
@@ -70,42 +73,42 @@ public class RobotShoot {
         a = timerRelatch.get();
     }*/
 
-    private static int getEncoderValue() {
-        revolutionsOfShooter = RobotSensors.shooterEncoder.get();
+    private static int getEncoderValue() {  //this gets the encoder value to be used by the unwindShooter method
+        revolutionsOfShooter = RobotSensors.SHOOTER_WHINCH_ENCODER.get();
         return revolutionsOfShooter;
     }
     
-    public static void unwindShooter() {
-        timerLatch = new Timer();
-        while (!limitLatched) {
-            RobotSensors.motorShooter.set(UNIWIND_SPEED);
-            if(RobotShoot.getEncoderValue()>= rewindMaxRevolutions){
-                RobotSensors.motorShooter.set(0);
+    public static void unwindShooter() {    //this uwnids the shooter     
+        boolean getOut = true;
+        while (!limitLatched || !getOut) {
+            RobotActuators.SHOOTER_WINCH.set(UNIWIND_SPEED);
+            if(RobotShoot.getEncoderValue()== rewindMaxRevolutions){
+                RobotActuators.SHOOTER_WINCH.set(0);
             }
-            double a = timerLatch.get();
-            if(a>500){
-                RobotSensors.motorShooter.set(0);
-                RobotShoot.rewindShooter();
+            time = timerLatch.get();
+            if(time>500){
+                RobotActuators.SHOOTER_WINCH.set(0);
                 SmartDashboard.putString("Error", "The shooter is reloading, try Shooting again");
+                getOut = false;
             }
         }
     }
-    public static void rewindShooter() {
+    public static void rewindShooter() {  //this rewinds the shooter at the wind speed
         while (!limitBuckleValue) {
-            RobotSensors.motorShooter.set(WIND_SPEED);
+            RobotActuators.SHOOTER_WINCH.set(WIND_SPEED);
         }
     }
 
-    public static void update() {
+    public static void update() {  //this is the update method to get all of our values that are needed
         if (timerRelatch != null) {
             b = timerRelatch.get();
-            if (a - b > 500) {
-                RobotSensors.latchSolenoid.set(true);
+            if (time - b > 500) {
+                RobotActuators.LATCH.set(true);
                 timerRelatch = null;
             }
         }
-        limitBuckleValue = RobotSensors.limitBuckle.get();
-        limitShooter = RobotSensors.limitShooter.get();    
-        limitLatched = RobotSensors.limitLatched.get();
+        limitBuckleValue = RobotSensors.SHOOTER_LOADED_LIM.get();   //SHOOTER_LOADED_LIM
+        limitShooter = RobotSensors.SHOOTER_UNLOADED_LIM.get();    //SHOOTER_UNLOADED_LIM
+        limitLatched = RobotSensors.SHOOTER_LATCHED_LIM.get();   //SHOOTER_LATCHED_LIM
     }
 }
