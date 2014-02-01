a8bbef12c19bd24d2a355b82746b9e71f5ae12ba
jdurha0013 <durhamj8@msu.edu>
2014-02-01 10:18:53 -0800

    RobotPickUp
    lowered encoder value in test method for manual rotation of pickup

--- FRC-2014/src/edu/wpi/first/wpilibj/templates/RobotPickUp.java
+++ FRC-2014/src/edu/wpi/first/wpilibj/templates/RobotPickUp.java
@@ -40,11 +40,12 @@ public class RobotPickUp {
         ballInPickUpLimit = RobotSensors.ballReadyToLiftLim.get();
         lowerLimit = RobotSensors.pickupSystemDownLim.get();
         RobotActuators.pickupRollerArmMotor.set(rollerMotorSpeed);
-        RobotActuators.leftDrive.set(armMotorSpeed);
+        RobotActuators.pickupSystemMotor.set(armMotorSpeed);
         SmartDashboard.putBoolean("upperLimit", upperLimit);
         SmartDashboard.putBoolean("lowerLimit", lowerLimit);
         SmartDashboard.putBoolean("ballInPickUpLimit", ballInPickUpLimit);
-        //CHANGE LEFT DRIVE BACK TO PICKUPSYSTEMMOTOR AFTER TESTING
+        SmartDashboard.putNumber("Arm Encoder", armEncoder);
+
     }
     public static boolean ifLoaded() {
         return ballInPickUpLimit;
@@ -171,7 +172,7 @@ public class RobotPickUp {
 
     public static void test(boolean buttonTestA, boolean buttonTestB, boolean buttonTestC) {
         if (buttonTestA && !buttonTestB && !buttonTestC) {
-            moveToShoot(500, 1.0, true);
+            moveToShoot(180, 1.0, true);
         } else if (buttonTestB && !buttonTestA && !buttonTestC) {
             moveToBottomPosition();
         } else if (buttonTestC && !buttonTestA && !buttonTestB) {
