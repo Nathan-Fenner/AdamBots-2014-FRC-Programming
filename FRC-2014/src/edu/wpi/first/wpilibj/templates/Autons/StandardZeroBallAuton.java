d3ee97083077aa3aff6cb79a440475bc7b354eeb
Aang12345125 <jedi_tyler8@yahoo.com>
2014-02-08 19:59:33 -0500

    Standard 0 Ball Auton
    created the standard 0 ball auton

---
+++ FRC-2014/src/edu/wpi/first/wpilibj/templates/Autons/StandardZeroBallAuton.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package edu.wpi.first.wpilibj.templates.Autons;
+
+/**
+ *
+ * @author Tyler
+ */
+public class StandardZeroBallAuton {
+    public StandardZeroBallAuton() {
+	
+    }
+}
