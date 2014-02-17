917326ce4c4e8a913b786d0c63f6eb5b22b823bf
GabbyFung <gabbyfung99@gmail.com>
2014-02-17 10:42:43 -0500

    Added Swag Mode

--- FRC-2014/src/edu/wpi/first/wpilibj/templates/RobotLights.java
+++ FRC-2014/src/edu/wpi/first/wpilibj/templates/RobotLights.java
@@ -3,8 +3,8 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-
 package edu.wpi.first.wpilibj.templates;
+
 import edu.wpi.first.wpilibj.DigitalInput;
 import edu.wpi.first.wpilibj.Relay;
 
@@ -13,52 +13,100 @@ import edu.wpi.first.wpilibj.Relay;
  * @author Tyler
  */
 public class RobotLights {
-    
+
 ////VARIABLES-------------------------------------------------------------------
     private DigitalInput liar;
-    
-    
-    
+
 ////CONSTANTS-------------------------------------------------------------------
-    
     public void init() {
-	//INSERT CODE HERE
-                 
-        
-      
+        //INSERT CODE HERE
+
     }
-    
+
+    //randomly turning on and off lights
+    public void swagMode() {
+        int strand = (int) MathUtils.rand(6) + 1;
+        switch (strand) {
+            case 1:
+                if (RobotActuators.groundLEDStrip1.get() == Relay.Value.kOn) {
+                    RobotActuators.groundLEDStrip1.set(Relay.Value.kOff);
+                } else {
+                    RobotActuators.groundLEDStrip1.set(Relay.Value.kOn);
+                }
+                break;
+            case 2:
+                if (RobotActuators.groundLEDStrip2.get() == Relay.Value.kOn) {
+                    RobotActuators.groundLEDStrip2.set(Relay.Value.kOff);
+                } else {
+                    RobotActuators.groundLEDStrip2.set(Relay.Value.kOn);
+                }
+
+                break;
+            case 3:
+                if (RobotActuators.groundLEDStrip3.get() == Relay.Value.kOn) {
+                    RobotActuators.groundLEDStrip3.set(Relay.Value.kOff);
+                } else {
+                    RobotActuators.groundLEDStrip3.set(Relay.Value.kOn);
+                }
+                break;
+            case 4:
+                if (RobotActuators.groundLEDStrip4.get() == Relay.Value.kOn) {
+                    RobotActuators.groundLEDStrip3.set(Relay.Value.kOff);
+                } else {
+                    RobotActuators.groundLEDStrip4.set(Relay.Value.kOn);
+                }
+
+                break;
+            case 5:
+                if (RobotActuators.LEDStripLeftPanel.get() == Relay.Value.kOn) {
+                    RobotActuators.LEDStripLeftPanel.set(Relay.Value.kOff);
+                } else {
+                    RobotActuators.LEDStripLeftPanel.set(Relay.Value.kOn);
+                }
+                break;
+            case 6:
+                if (RobotActuators.LEDStripRightPanel.get() == Relay.Value.kOn) {
+                    RobotActuators.LEDStripRightPanel.set(Relay.Value.kOff);
+                } else {
+                    RobotActuators.LEDStripRightPanel.set(Relay.Value.kOn);
+                }
+                break;
+            default:
+                System.out.println("rand: " + strand);
+                break;
+        }
+    }
+
     public void shootingLights() {
-	//INSERT CODE HERE
+        //INSERT CODE HERE
     }
-    
+
     public boolean ifReady() {
-	//INSERT CODE HERE
-	return true;
-	}
-    
+        //INSERT CODE HERE
+        return true;
+    }
+
     public boolean ifJammed() {
-	//INSERT CODE HERE
-	return true;
-	}
-    
+        //INSERT CODE HERE
+        return true;
+    }
+
     public void lightResponse() {
-	boolean ifReady = ifReady();
-	boolean ifJammed = ifJammed();
-	if(ifReady)
-	    ;
-	else
+        boolean ifReady = ifReady();
+        boolean ifJammed = ifJammed();
+        if (ifReady)
+	    ; else
 	    ; //Error message
-	if(ifJammed)
+        if (ifJammed)
 	    ; //Error message
-	else
+        else
 	    ;
 
-	//INSERT CODE HERE
+        //INSERT CODE HERE
     }
-    
+
     public void underglowOn() {
-	//INSERT CODE HERE
+        //INSERT CODE HERE
         // turns on
         // RobotActuators.groundLEDStrip1.set(Relay.Value.kOn);
         // turns off
@@ -67,36 +115,35 @@ public class RobotLights {
         RobotActuators.groundLEDStrip2.set(Relay.Value.kOn);
         RobotActuators.groundLEDStrip3.set(Relay.Value.kOn);
         RobotActuators.groundLEDStrip4.set(Relay.Value.kOn);
-           
+
     }
-    
-    public void underglowOff(){
-       RobotActuators.groundLEDStrip1.set(Relay.Value.kOff);
-       RobotActuators.groundLEDStrip2.set(Relay.Value.kOff);
-       RobotActuators.groundLEDStrip3.set(Relay.Value.kOff);
-       RobotActuators.groundLEDStrip4.set(Relay.Value.kOff);
+
+    public void underglowOff() {
+        RobotActuators.groundLEDStrip1.set(Relay.Value.kOff);
+        RobotActuators.groundLEDStrip2.set(Relay.Value.kOff);
+        RobotActuators.groundLEDStrip3.set(Relay.Value.kOff);
+        RobotActuators.groundLEDStrip4.set(Relay.Value.kOff);
     }
-    
+
     public void cameraSpotlight() {
-	//INSERT CODE HERE
+        //INSERT CODE HERE
     }
-    
+
     public void confuse() {
-	//INSERT CODE HERE
-	for(int loop = 0; loop < 100; loop++)
-	{
-	    //turn on
-	    //turn off
-	    delay(50);
-	}
+        //INSERT CODE HERE
+        for (int loop = 0; loop < 100; loop++) {
+            //turn on
+            //turn off
+            delay(50);
+        }
     }
-    
+
     public void delay(int time) {
-	try {
-	    Thread.sleep(time);
-	} catch (InterruptedException e) {
-	    System.out.println(e);
-	
+        try {
+            Thread.sleep(time);
+        } catch (InterruptedException e) {
+            System.out.println(e);
+
         }
     }
 
