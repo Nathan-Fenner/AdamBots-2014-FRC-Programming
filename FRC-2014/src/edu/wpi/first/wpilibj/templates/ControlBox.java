f920bf08bb8cd776b0954ece100f85da4619ca7c
Nathan Fenner <nfenneremail@gmail.com>
2014-02-20 15:36:59 -0500

    Updated MagicBox to refer to switches

--- FRC-2014/src/edu/wpi/first/wpilibj/templates/ControlBox.java
+++ FRC-2014/src/edu/wpi/first/wpilibj/templates/ControlBox.java
@@ -13,18 +13,17 @@ import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
  */
 public class ControlBox {
 	/*
-	 /           #1       #2     #3
+	 .	#9	#8	#7
 
-	 #4            B1            B2
+	 #10I	#3I		#4I
 
-	 #5
+	 #11I
 
-	 #6            B1            B2
+	 #12I	#5I		#6I
 
-	 #7
+	 #13I
 
-	 #8            B1            B2
-
+	 #14I	#1I		#2I
 	 */
 
 	public static DriverStation driverStation;
@@ -40,9 +39,52 @@ public class ControlBox {
 	public static double getAnalogIn(int channel) {
 		return driverStation.getAnalogIn(channel);
 	}
+
 	public static void update() {
 		for (int i = 1; i <= 14; i++) {
-			SmartDashboard.putBoolean("Switch " + i,driverStation.getDigitalIn(i));
+			SmartDashboard.putBoolean("Switch " + i, driverStation.getDigitalIn(i));
 		}
+	}
+
+	public static boolean getYellowButtonUpLeft() {
+		return !getDigitalIn(3);
+	}
+
+	public static boolean getYellowButtonUpRight() {
+		return !getDigitalIn(4);
+	}
+
+	public static boolean getYellowButtonDownLeft() {
+		return !getDigitalIn(5);
+	}
+
+	public static boolean getYellowButtonDownRight() {
+		return !getDigitalIn(6);
+	}
+
+	public static boolean getBlackButtonLeft() {
+		return !getDigitalIn(1);
+	}
+
+	public static boolean getBlackButtonRight() {
+		return !getDigitalIn(2);
+	}
+
+	/**
+	 *
+	 * @param which numbered from top to bottom (1-5)
+	 * @return
+	 */
+	public static boolean getLeftSwitch(int which) {
+		return !getDigitalIn(9 + which);
+	}
+
+	/**
+	 *
+	 * @param which from left to right (1-3)
+	 * @return
+	 */
+	public static boolean getTopSwitch(int which) {
+		return getDigitalIn(10 - which);
 	}
 }
